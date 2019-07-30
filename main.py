39d6326ff47bbe50932b770a926e446e515c3994
ash4995 <yash95shah@gmail.com>
2019-07-29 23:07:27 -0700

    Added the html boilerplate for the front end!

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "<h1 style=font-family: times;>This is the first commit!</h1>"
+    return "<h1 style=font-famidddddddly: times;>This is the first commit!</h1> <p> You have only three lives. If you wish to continue, you need to form good habits! <br> <ul><li> Go strong </li> <li> Be bold </li>  <li> Take your chances </li></ul> "
+
 
 @app.route("/about")
 def about():
-    return "This is just gonna have to work"
+    return "< h3 style = font- family : helvetica ; > This is just gonna have to work < /h3 >"
 
 
 @app.route("/documentation")
