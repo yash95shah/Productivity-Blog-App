d28228c0937554daf5eb8be823f43d4fa1a1ff8d
ash4995 <yash95shah@gmail.com>
2019-07-31 12:30:43 -0700

    Created a layout for the forms.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '8e54af86ebd2a0e82932'
 posts = [
    { 'author' : 'Yash Shah', 'Designation' : 'Software Engineer' } ,
     {
@@ -24,8 +25,19 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/documentation")
-def documentation():
-    return "This is a sample documentation page"
+@app.route("/registration")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title= 'Login', form=form)
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
