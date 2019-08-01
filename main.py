5955d24e3afaa73c7c2745a7a48d4cbed5cc6306
ash4995 <yash95shah@gmail.com>
2019-08-01 16:44:30 -0700

    Made the registration page fully functioning!

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+<<<<<<< HEAD
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+=======
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app=app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    date_posted=db.Column(db.DateTime, nullable=False, default=datetime.time())
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}' "
+
+>>>>>>> Made the registration page fully functioning!
 
 posts = [
     {
@@ -19,7 +51,11 @@ posts = [
     }
 ]
 
+<<<<<<< HEAD
 
+=======
+#create home route
+>>>>>>> Made the registration page fully functioning!
 @app.route("/")
 @app.route("/home")
 def home():
@@ -36,7 +72,11 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
+<<<<<<< HEAD
         return redirect(url_for('home'))
+=======
+        return redirect(url_for('register'))
+>>>>>>> Made the registration page fully functioning!
     return render_template('register.html', title='Register', form=form)
 
 
