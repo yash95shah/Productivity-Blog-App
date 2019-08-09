47ccce36fe750461367dcf9464053640ee87c53f
ash4995 <yash95shah@gmail.com>
2019-08-09 09:55:06 -0700

    Made each post a link with a reference to its post_id -

--- FlaskBlog/models.py
+++ FlaskBlog/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"{self.username}"
 
 
 class Post(db.Model):
--- FlaskBlog/routes.py
+++ FlaskBlog/routes.py
@@ -3,36 +3,24 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    users = User.query.all()
+    return render_template('about.html', title='About', users=users)
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -99,9 +87,29 @@ def account():
         db.session.commit()
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
-    elif request.method == 'GET':
+    elif current_user.is_authenticated and not current_user.is_anonymous:
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET','POST'])
+@login_required
+def new_post():
+    form  = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content= form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        
+        flash('Your post has been created!','success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form= form)
+
+
+@app.route("/post/<post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
